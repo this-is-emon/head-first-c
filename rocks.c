3e7d1cddcde7e8cc10f9654784183c38c63e8d50
this-is-emon <mofijul.islam.emon@gmail.com>
2024-04-12 14:53:54 +0600

    json file and first prograam updated.

---
+++ rocks.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+    puts("C Rocks!");
+    return 0;
+}
+
+/*
+-- Choose puts when you need to print plain strings with a newline and don't need formatting. 
+-- Choose printf for complex outputs involving various data types, formatted numbers, 
+   or when precise control over the output appearance is necessary.
+*/
